30b8da846bddd142e3e9b4b22fb123be74411c45
kvu-luong <luongkhanhvu1392@gmail.com>
2019-06-27 13:19:28 +0700

    add alaska project

--- wp-content/themes/housing/template-parts/content-page.php
+++ wp-content/themes/housing/template-parts/content-page.php
@@ -11,7 +11,7 @@
 <div class="container">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
-			<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+			<!-- <?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?> -->
 		</header><!-- .entry-header -->
 		<div class="row">
 			<?php
